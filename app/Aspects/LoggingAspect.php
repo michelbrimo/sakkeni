b4f998c1bcf007b0d623cc60bd695362106e1b52
Michel Brimo <brimomichel@gmail.com>
2025-08-30 15:56:43 +0300

    fix the label in logger

--- app/Aspects/LoggingAspect.php
+++ app/Aspects/LoggingAspect.php
@@ -8,11 +8,33 @@ use Illuminate\Support\Facades\Validator;
 
 class LoggingAspect
 {   
+
+    protected $operationsLabels = [
+        'updateUserProfile' => 'Update the profile',
+        'resetPassword' => 'Reset the passwrod',
+        'upgradeToSeller' => 'Upgrade to seller',
+        'addProperty' => 'Add property',
+        'updateProperty' => 'Update property',
+        'propertySold' => 'Sold property',
+        'deleteProperty' => 'Delete property',
+        'addPropertyToFavorite' => 'Add property to favorite',
+        'removePropertyFromFavorite' => 'Remove property from favorite',
+
+        'upgradeToServiceProvider' => 'Upgrade to service provider',
+        'addService' => 'Add service',
+        'removeService' => 'Remove service',
+        'editService' => 'Edit service',
+
+        'reportProperty' => 'Report property',
+        'reportServiceProvider' => 'Report service provider',
+    ];
+
+
     public function before($operation){
         Log::create([
             "user_id" => auth()->user() != null ? auth()->user()->id : null,
             "name" => auth()->user() != null ? auth()->user()->first_name . ' ' . auth()->user()->last_name : 'unregistered user',
-            "operation" => $operation,
+            "operation" => $this->operationsLabels[$operation],
             "status" => "Started",
         ]);
     }
@@ -22,7 +44,7 @@ class LoggingAspect
         Log::create([
             "user_id" => auth()->user() != null ? auth()->user()->id : null,
             "name" => auth()->user() != null ? auth()->user()->first_name . ' ' . auth()->user()->last_name : 'unregistered user',
-            "operation" => $operation,
+            "operation" => $this->operationsLabels[$operation],
             "status" => "Finished",
         ]);
     }
@@ -31,7 +53,7 @@ class LoggingAspect
         Log::create([
             "user_id" => auth()->user() != null ? auth()->user()->id : null,
             "name" => auth()->user() != null ? auth()->user()->first_name . ' ' . auth()->user()->last_name : 'unregistered user',
-            "operation" => $operation,
+            "operation" => $this->operationsLabels[$operation],
             "status" => "Exception",
         ]);
     }
